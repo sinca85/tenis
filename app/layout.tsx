64454d4697429beaf169160f38bd19a990af4180
Santiago Acosta Villa Abrille <acosta.s@octubre.org.ar>
2026-09-01 15:14:45 -0300

    Prevent search engine indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Tenis | Santivillabrile",
   description: "Disponibilidad de canchas de tenis en Neptunia",
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
